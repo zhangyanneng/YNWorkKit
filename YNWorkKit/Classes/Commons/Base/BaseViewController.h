2e05a2268c7c9e29e46bdb1ccac4a35b5760a56a
zyn <1787973358@qq.com>
2016-09-11 18:02:19 +0800

    添加基础baseViewController

---
+++ YNWorkKit/Classes/Commons/Base/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  YNWorkKit
+//
+//  Created by zyn on 16/9/11.
+//  Copyright © 2016年 张艳能. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
---
+++ YNWorkKit/Classes/Commons/Base/BaseViewController.m
@@ -0,0 +1,31 @@
+//
+//  BaseViewController.m
+//  YNWorkKit
+//
+//  Created by zyn on 16/9/11.
+//  Copyright © 2016年 张艳能. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface BaseViewController ()
+
+@end
+
+@implementation BaseViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+
+
+@end
